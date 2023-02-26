f626f6983b3affda3580a688f1a44b81a02a7e62
Stuart Millman <stu.millman15@gmail.com>
2023-02-26 14:45:11 +0000

    Add frame time graph.

--- App/src/core/application.cpp
+++ App/src/core/application.cpp
@@ -17,6 +17,57 @@ static bool g_isAppRunning;
 
 static app::core::Application* s_Instance = nullptr;
 
+struct ScrollingBuffer
+{
+    app::u32 MaxSize;
+    app::u32 Offset;
+    ImVector<ImVec2> Data;
+    ScrollingBuffer(int max_size = 2000)
+    {
+        MaxSize = max_size;
+        Offset = 0;
+        Data.reserve(MaxSize);
+    }
+    void AddPoint(float x, float y)
+    {
+        if (Data.size() < static_cast<app::sizet>(MaxSize))
+            Data.push_back(ImVec2(x, y));
+        else
+        {
+            Data[Offset] = ImVec2(x, y);
+            Offset = (Offset + 1) % MaxSize;
+        }
+    }
+    void Erase()
+    {
+        if (Data.size() > 0)
+        {
+            Data.shrink(0);
+            Offset = 0;
+        }
+    }
+};
+
+void draw_cpu_frame_graph(app::f32 time, app::f32 delta_time)
+{
+    static ScrollingBuffer data{};
+
+    data.AddPoint(time, delta_time);
+
+    static float history = 10.0f;
+
+    static ImPlotAxisFlags flags = ImPlotAxisFlags_NoLabel | ImPlotAxisFlags_NoTickMarks | ImPlotAxisFlags_NoTickLabels;
+    if (ImPlot::BeginPlot("##graph_frametime_cpu", ImVec2(-1, 150)))
+    {
+        ImPlot::SetupAxes(nullptr, nullptr, flags, flags);
+        ImPlot::SetupAxisLimits(ImAxis_X1, time - history, time, ImGuiCond_Always);
+        ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 1);
+        ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL, 0.5f);
+        ImPlot::PlotLine("Frame Time", &data.Data[0].x, &data.Data[0].y, data.Data.size(), 0, data.Offset, 2 * sizeof(float));
+        ImPlot::EndPlot();
+    }
+}
+
 namespace app::core
 {
     Application::Application(const ApplicationInfo& app_info) : m_appInfo(app_info)
@@ -84,6 +135,8 @@ namespace app::core
             {
                 ImGui::Text("Frame Time: %ims / %ifps", static_cast<u32>(m_deltaTime * 1000.0f), m_fps);
                 ImGui::Text("Memory Usage (bytes): %i", GetAllocationMetrics().CurrentUsage());
+
+                draw_cpu_frame_graph(get_time(), get_delta_time());
             }
 
             m_renderer.end_frame();
